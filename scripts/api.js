9c1d2f8e73173e7a901d6a5b203d3a94c3541ab7
Aveson <avesoncarvalho25@gmail.com>
2023-09-30 11:24:08 +0100

    added working stats and some mroe design

--- scripts/api.js
+++ scripts/api.js
@@ -9,31 +9,67 @@ btnSearch.addEventListener("click",()=>{
     fetch(url)
     .then(data => data.json())
     .then(data =>{
+      // Variables for pokemon data
+        //pokemon image
         const pokemonImage = document.querySelector(".pokemon-image");
         pokemonImage.innerHTML = `<img src="${data.sprites.front_default}">`
-        const tempNameElement = document.querySelector("#name");
-        tempNameElement.textContent = `Name: ${data.name}`;
+
+        // Pokemon name
+        const name = document.querySelector("#name");
+        name.textContent = `Name: ${data.name.toUpperCase()}`;
+
+        // Pokemon name
+        const id = document.querySelector("#id");
+        id.textContent = `ID: #${data.id}`;
+        
+        // Pokemon height
         const height = document.querySelector("#height");
-        height.textContent = `${data.height}m`
+        height.textContent = `Height: ${data.height}m`
+
+        // Pokemon weight
         const weight = document.querySelector("#weight");
-        weight.textContent = `${data.weight}kg`
+        weight.textContent = `Weight: ${data.weight}kg`
     
+        // pokemon types
         const types = document.querySelector("#types");
         
+        // Loop for all types to be displayed
         for (let index = 0; index < data.types.length; index++) {
-            types.innerHTML += `<span class="${data.types[index].type.name}">${data.types[index].type.name.toUpperCase()}</span>`;
-        }
-    
-        const stats = document.querySelector("#stats");
-        for (let index = 0; index < data.stats.length; index++) {
-    
-            let name = data.stats[index].stat.name.toUpperCase();
-            let percentage = (data.stats[index].base_stat / 450) * 100
-            stats.innerHTML += `<div>${name}</div>
-          <div class="progdatas" role="progdatasbar" aria-label="Example 20px high" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="height: 20px">
-            <div class="progdatas-bar" style="width: ${percentage}%;">${data.stats[index].base_stat}</div>
-          </div>`
+          types.innerHTML += `<span class="${data.types[index].type.name}">${data.types[index].type.name.toUpperCase()}</span>`;
         }
         
+        // Display stats
+        const stats = document.querySelector("#stats");
+        stats.innerHTML += `<hr class="hr">`
+        for (let index = 0; index < data.stats.length; index++) {
+          // Makes a progress bar with stats
+          let name = data.stats[index].stat.name.toUpperCase();
+          let percentage = (data.stats[index].base_stat / 450) * 100
+          // change stats progressbar colour
+        // change stats progressbar colour
+        let extraClass = "bg-success";
+        
+        // change extraclass based on name
+        if (name === "ATTACK") {
+          extraClass = "bg-danger";
+      } else if (name === "SPECIAL-ATTACK") {
+          extraClass = "bg-danger";
+      } else if (name === "DEFENSE" ||name === "SPECIAL-DEFENSE") {
+          extraClass = "bg-info";
+      } else if (name === "SPEED") {
+          extraClass = "bg-warning";
+      } else {
+          console.log(`Unknown case; ${name}`);
+      }
+      
+          // output the stats
+          stats.innerHTML += `<div class="stats-name">${name}</div><div class="progress" role="progressbar" aria-label="Example 20px high" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="height: 20px">
+          <div class="progress-bar ${extraClass} progress-text progress-bar-striped progress-bar-animated" style="width: ${percentage}%">${data.stats[index].base_stat}</div>
+        </div>`
+        console.log(name,extraClass)
+        }
     })
 })
+
+
+// HP, ATTACK, SPECIAL ATTACK, SPECIAL DEFENSE, SPEED
